3835e6001f7197b7ff0a0923dcf1f354185ae58b
IBM-DLiu <dliu@us.ibm.com>
2026-04-11 16:34:20 -0400

    Delete 09_PBFD_MVP directory

--- 09_PBFD_MVP/Services/ILocationResetService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VisitorLog_PBFD.Services
-{
-    public interface ILocationResetService
-    {
-        Task ResetTableColumnsAsync(string parentTable, List<(int ChildId, int LocationId)> currSelectedContinents, int personId);
-    }
-}
--- 09_PBFD_MVP/ViewModels/ChildViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VisitorLog_PBFD.ViewModels
-{
-    public class ChildViewModel
-    {
-        public int Id { get; set; }
-        public int ChildId { get; set; }
-        public required string Name { get; set; }
-    }
-}
